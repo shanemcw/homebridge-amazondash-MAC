c9a71d900ebc4c5f19d9713524d1c375073f55ca
shanemcw <shane.mcwhorter@uxphd.co>
2021-03-12 14:00:19 -0500

    Updates for output of config.schema.json

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function DashPlatform(log, config, api) {
   self.log          = log;
   self.config       = config                   || { "platform": "AmazonDash-MAC" };
   self.buttons      = self.config.buttons      || [];
-  self.timeout      = self.config.timeout      || 9000; 
+  self.timeout      = self.config.timeout      || 9000; // greater than --berlin * 1000
   self.debug        = self.config.debug        || 1; // 0-3, 10
   self.manufacturer = self.config.manufacturer || "Amazon";
   
@@ -29,7 +29,7 @@ function DashPlatform(log, config, api) {
   self.accessories = {};
 
   self.airodump = null;
-  
+
   if (api) {
     self.api = api;
     self.api.on('didFinishLaunching', self.didFinishLaunching.bind(this));
@@ -45,9 +45,9 @@ DashPlatform.prototype.configureAccessory = function(accessory) {
     self.log("ERROR: configureAccessory called for malformed accessory (e.g. \"MAC\") missing");
     return;
     }
-  
+
   accessory.reachable = true;
-  
+
   accessory.context.lastTriggered = null;
 
   accessory
@@ -62,11 +62,11 @@ DashPlatform.prototype.configureAccessory = function(accessory) {
     .getService(Service.StatelessProgrammableSwitch)
     .getCharacteristic(Characteristic.ProgrammableSwitchEvent)
     .setProps({minValue: 0, maxValue: 0, validValues: [0]});
-  
+
   self.accessories[accessory.context.mac] = accessory;
-  
-  self.alias[accessory.context.mac] = accessory.context.mac;
-  
+
+  self.alias[accessory.context.mac] = accessory.context.mac; // self-referential
+
   // optional aliasing
   if (accessory.context.alias) {
     for (var i in accessory.context.alias) {
@@ -85,27 +85,27 @@ DashPlatform.prototype.didFinishLaunching = function() {
     for (var m in self.accessories) { self.removeAccessory(self.accessories[m]); }
     self.debug = 2;
     }
- 
+
   for (var i in self.buttons) {
     if (!self.buttons[i].MAC) {
       self.log("ERROR: required accessory settings (e.g. \"MAC\") missing");
       return;
       }
-    
+
     if (!self.accessories[self.buttons[i].MAC]) {
-      self.addAccessory(self.buttons[i]); 
+      self.addAccessory(self.buttons[i]);
       }
-  }
+    }
 
   if (Object.keys(self.accessories).length > 0) {
     if (self.debug >= 1) { self.log("airodump-ng starting on " + self.config.interface + " and channel " + self.config.channel); }
-    
+
     self.airodump = spawn('airodump-ng', [self.config.interface, '--channel', self.config.channel, '--berlin', 1]);
-    
+
     self.airodump.stdout.on('data', function(data) { self.handleOutput(self, data); });
     self.airodump.stderr.on('data', function(data) { self.handleOutput(self, data); });
     self.airodump.on('close', function(code) { self.log('airodump-ng ended, code ' + code); });
-      
+
     if (self.debug >= 1) { self.log("airodump-ng started."); }
   }
 }
@@ -119,9 +119,9 @@ DashPlatform.prototype.handleOutput = function(self, data) {
       if (matches != null && matches.length > 0) {
         if (self.debug >= 3) { self.log("parsed MAC " + matches[0]); }
         // additional macs can masquerade as the accessory mac
-        var accessory = self.accessories[self.alias[matches[0]]]; 
+        var accessory = self.accessories[self.alias[matches[0]]];
         // also rate limit triggers
-        if (accessory && (accessory.context.lastTriggered == null || Math.abs((new Date()) - accessory.context.lastTriggered) > self.timeout)) { 
+        if (accessory && (accessory.context.lastTriggered == null || Math.abs((new Date()) - accessory.context.lastTriggered) > self.timeout)) {
           if (self.debug >= 1) { self.log("triggering " + accessory.displayName + " from " + matches[0]); }
           accessory.context.lastTriggered = new Date();
           self.dashEventWithAccessory(self, accessory);
@@ -138,34 +138,34 @@ DashPlatform.prototype.dashEventWithAccessory = function(self, accessory) {
     .setValue(0); // 0 = single press event
 }
 
-DashPlatform.prototype.addAccessory = function(button) {   
+DashPlatform.prototype.addAccessory = function(button) {
   if (this.debug >= 2) { this.log("addAccessory " + button.MAC  + " as " + button.name); }
-  
+
   if (!button.MAC) {
     self.log("ERROR: addAccessory called without required accessory settings (e.g. \"MAC\" missing)");
     return;
     }
- 
+
   var uuid = UUIDGen.generate(button.MAC);
 
   var newAccessory = new Accessory(button.name, uuid, 15); // PROGRAMMABLE_SWITCH_TCTYPE
-  
+
   newAccessory.reachable = true;
-  
+
   newAccessory.context.lastTriggered = null;
-  
+
   newAccessory.context.mac = button.MAC;
-  
-  if (button.alias) {
+
+  if (button.alias && (button.alias.length > 0)) {
     newAccessory.context.alias = button.alias;
-    }
-    
+  }
+
   newAccessory.context.serial   = button.serial   || 0;
   newAccessory.context.firmware = button.firmware || 0;
   newAccessory.context.model    = button.model    || 0;
- 
+
   newAccessory.addService(Service.StatelessProgrammableSwitch, button.name);
-    
+
   newAccessory
     .getService(Service.AccessoryInformation)
     .setCharacteristic(Characteristic.Manufacturer,     this.manufacturer)
@@ -177,30 +177,30 @@ DashPlatform.prototype.addAccessory = function(button) {
     .getService(Service.StatelessProgrammableSwitch)
     .getCharacteristic(Characteristic.ProgrammableSwitchEvent)
     .setProps({minValue: 0, maxValue: 0, validValues: [0]});
-    
+
   this.accessories[newAccessory.context.mac] = newAccessory;
-  
+
   this.alias[newAccessory.context.mac] = newAccessory.context.mac; // primary accessory is its own alias
-  
+
   if (newAccessory.context.alias) {
     // additional aliases optional
     for (var i in newAccessory.context.alias) {
       if (this.debug >= 2) { this.log(button.name + " also responding to " + newAccessory.context.alias[i]); }
-      this.alias[newAccessory.context.alias[i]] = newAccessory.context.mac;  
+      this.alias[newAccessory.context.alias[i]] = newAccessory.context.mac;
       }
     }
-  
+
   this.api.registerPlatformAccessories("homebridge-amazondash-mac", "AmazonDash-MAC", [newAccessory]);
 }
 
-DashPlatform.prototype.removeAccessory = function(accessory) { 
+DashPlatform.prototype.removeAccessory = function(accessory) {
   if (!accessory.context.mac) {
     self.log("ERROR: removeAccessory called for malformed accessory (e.g. \"MAC\" missing)");
     return;
     }
-    
+
   if (this.debug >= 2) { this.log("removeAccessory " + accessory.displayName); }
-  
+
   if (accessory) {
     this.api.unregisterPlatformAccessories("homebridge-amazondash-mac", "AmazonDash-MAC", [accessory]);
     delete this.accessories[accessory.context.mac];
@@ -208,4 +208,3 @@ DashPlatform.prototype.removeAccessory = function(accessory) {
 }
 
 DashPlatform.prototype.configurationRequestHandler = function(context, request, callback) { }
-
