b1e8ee51c2f72daa05a93fa1a25cbf2e8feaf40e
Shane McWhorter <shane@mcwhorter.me>
2022-03-22 21:03:42 -0400

    version 3.0.0

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 //   forked from jourdant/homebridge-amazondash-ng
 //    forked from KhaosT/homebridge-amazondash
 
-var spawn = require('child_process').spawn;
+const express = require('express');
+const spawn   = require('child_process').spawn;
+
 var Accessory, Service, Characteristic, UUIDGen;
 
 module.exports = function(homebridge) {
@@ -18,7 +20,7 @@ function DashPlatform(log, config, api) {
   self.log          = log;
   self.config       = config                   || { "platform": "AmazonDash-MAC" };
   self.buttons      = self.config.buttons      || [];
-  self.timeout      = self.config.timeout      || 9000; // rate limit greater than connection attempt time in ms
+  self.timeout      = self.config.timeout      || 7500; // rate limit greater than connection attempt time in ms
   self.debug        = self.config.debug        || 1; // 0-4, 10
   self.manufacturer = self.config.manufacturer || "Amazon";
   self.alias        = {}; // additional MACs can masquerade as accessory MAC via this alias map
@@ -29,6 +31,10 @@ function DashPlatform(log, config, api) {
     self.api = api;
     self.api.on('didFinishLaunching', self.didFinishLaunching.bind(this));
   }
+  if (self.config.wport) {
+    self.wapi  = express();
+    self.wport = self.config.wport;
+    }
 }
 
 DashPlatform.prototype.configureAccessory = function(accessory) {
@@ -47,10 +53,11 @@ DashPlatform.prototype.configureAccessory = function(accessory) {
     .setCharacteristic(Characteristic.FirmwareRevision, accessory.context.firmware)
     .setCharacteristic(Characteristic.SerialNumber,     accessory.context.serial);
   // single press is 0; double is 1, long press is 2)
+  let d = (accessory.context.doublePress) ? 1 : 0;
   accessory
     .getService(Service.StatelessProgrammableSwitch)
     .getCharacteristic(Characteristic.ProgrammableSwitchEvent)
-    .setProps({minValue: 0, maxValue: 1, validValues: [0, 1]});
+    .setProps({minValue: 0, maxValue: d, validValues: [0, d]});
   self.accessories[accessory.context.mac] = accessory;
   self.alias[accessory.context.mac] = accessory.context.mac; // self-referential
   // optional aliasing
@@ -65,32 +72,84 @@ DashPlatform.prototype.configureAccessory = function(accessory) {
 
 DashPlatform.prototype.didFinishLaunching = function() {
   var self = this;
+  
   if (!self.config.interface) {
     self.log(`\x1b[31m[ERROR]\x1b[0m required plugin settings (e.g. "interface") are not yet specified`);
     return;
     }
+    
   if (self.debug == 10) {
     self.log(`\x1b[33m[DEBUG 10]\x1b[0m removing all cached accessories and recreating from current settings`);
     self.log(`\x1b[33m[DEBUG 10]\x1b[0m change debug level to other than 10 and restart homebridge`);
     for (let a of Object.values(self.accessories)) { self.removeAccessory(a); }
     self.debug = 2;
     }
+    
   for (let b of self.buttons) {
     if (!b.MAC) {
       self.log(`\x1b[31m[ERROR]\x1b[0m required button settings (e.g. "MAC") are not yet specified`);
       return;
       }
     b.MAC = b.MAC.toUpperCase().replace(/([\dA-F]{2}\B)/g, "$1:");
-    if (!self.accessories[b.MAC]) { self.addAccessory(b); }
+    if (!self.accessories[b.MAC]) { 
+        self.addAccessory(b); 
+    } else if (b.doublePress !== self.accessories[b.MAC].context.doublePress) {
+        self.removeAccessory(self.accessories[b.MAC]);
+        self.addAccessory(b);
+        }
+    
+    // self.log(`\x1b[33m[DEBUG]\x1b[0m ${self.accessories[b.MAC].displayName} ${b.doublePress} ${self.accessories[b.MAC].context.doublePress}`);
     }
+    
   if (Object.keys(self.accessories).length > 0) {
     self.wifidump = spawn('sudo', ['tcpdump', '-i', self.config.interface, '--immediate-mode', '--monitor-mode', '-t', '-S', '-q', '-N', '-l', '-e', 'broadcast']);
-    self.wifidump.stdout.on('data', function(data) { self.handleOutput(self, data);  });
-    self.wifidump.stderr.on('data', function(data) { self.handleError(self, data);   });
+    self.wifidump.stdout.on('data', function(data) { self.handleOutput(self, data); });
+    self.wifidump.stderr.on('data', function(data) { self.handleError(self, data);  });
     self.wifidump.on('exit',  (code) => { self.log(`\x1b[31m[ERROR]\x1b[0m tcpdump exited, code ${code}`); });
     self.wifidump.on('close', (code) => { self.log(`\x1b[31m[ERROR]\x1b[0m tcpdump closed, code ${code}`); });
     self.wifidump.on('error', (err)  => { self.log(`\x1b[31m[ERROR]\x1b[0m tcpdump error ${err}`);         });
-  }
+    }
+  
+  if (self.wapi) {
+    self.wapi.get('/mac/:x', (req, res, next) => {
+      let m = req.params.x.toUpperCase().replace(/([\dA-F]{2}\B)/g, "$1:");
+      var btn;
+      let accessory = self.accessories[self.alias[m]];
+      if (accessory) {
+        for (let b of self.buttons) {
+          if (b.MAC == m) { btn = b; break; }
+          }
+        self.dashEventWithAccessory(self, accessory);
+        }
+      res.send(JSON.stringify(btn));  
+      });
+
+    self.wapi.get('/name/:x', (req, res, next) => {
+      var btn;
+      for (let b of self.buttons) {
+        if (b.name == req.params.x ) { btn = b; break; }
+        }
+      if (btn) {
+        let accessory = self.accessories[self.alias[btn.MAC]];
+        if (accessory) {
+          self.dashEventWithAccessory(self, accessory);
+          }
+        }
+      res.send(JSON.stringify(btn)); 
+      });
+      
+    self.wapi.get('/buttons', (req, res, next) => {
+      res.send(JSON.stringify(self.buttons)); 
+      });
+      
+    self.wapi.use((req, res, next) => {
+      res.send();  
+      });
+      
+    self.wapi.listen(self.wport, () => {
+      self.log(`Web API listening on port \x1b[4;97m${self.wport}\x1b[0m`);
+      });
+    }
 }
 
 DashPlatform.prototype.handleOutput = function(self, data) {
@@ -110,7 +169,7 @@ DashPlatform.prototype.handleOutput = function(self, data) {
               if (self.debug == 4) { self.log(`\x1b[33m[DEBUG 4]\x1b[0m MAC ${matches[0]} ${self.saw[matches[0]]}`); } // very verbose
            }   
         // additional MACs can masquerade as the accessory MAC
-        var accessory = self.accessories[self.alias[matches[0]]];
+        let accessory = self.accessories[self.alias[matches[0]]];
         // rate limit triggers less than connection attempt time
         if (accessory && (accessory.context.lastTriggered == null || Math.abs((new Date()) - accessory.context.lastTriggered) > self.timeout)) {
           if (self.debug >= 2) { self.log(`\x1b[4;97m${accessory.displayName}\x1b[0m triggered from ${matches[0]}`); }
@@ -138,7 +197,7 @@ DashPlatform.prototype.handleError = function(self, data) {
       if (/listening/.test(line)) { 
         let n = line.match(/on ([^\s,]+)/);
         if (n[1]) {
-          if (self.debug >= 1) { self.log(`now listening on \x1b[4;97m${n[1]}\x1b[0m`); }
+          if (self.debug >= 1) { self.log(`Wifi listening on interface \x1b[4;97m${n[1]}\x1b[0m`); }
           continue;
           }
         }
@@ -148,7 +207,7 @@ DashPlatform.prototype.handleError = function(self, data) {
     
 DashPlatform.prototype.dashEventWithAccessory = function(self, accessory) {
   let b = 0; let s = 'single'; // 0 = single, 1 = double, 2 = long press events
-  if (accessory.context.lastTriggered && Math.abs(new Date() - accessory.context.lastTriggered) < (self.timeout + 18000)) { b = 1; s = 'double'; } 
+  if (accessory.context.doublePress && accessory.context.lastTriggered && Math.abs(new Date() - accessory.context.lastTriggered) < (self.timeout + 18000)) { b = 1; s = 'double'; } 
   accessory
     .getService(Service.StatelessProgrammableSwitch)
     .getCharacteristic(Characteristic.ProgrammableSwitchEvent)
@@ -166,8 +225,9 @@ DashPlatform.prototype.addAccessory = function(button) {
   var uuid = UUIDGen.generate(button.MAC);
   var newAccessory = new Accessory(button.name, uuid, 15); // 15 = PROGRAMMABLE_SWITCH_TCTYPE
   newAccessory.reachable = true;
+  newAccessory.context.doublePress   = button.doublePress;
   newAccessory.context.lastTriggered = null;
-  newAccessory.context.mac = button.MAC;
+  newAccessory.context.mac           = button.MAC;
   if (button.alias && (button.alias.length > 0)) {
     newAccessory.context.alias = button.alias;
     }
@@ -181,10 +241,11 @@ DashPlatform.prototype.addAccessory = function(button) {
     .setCharacteristic(Characteristic.Model,            newAccessory.context.model)
     .setCharacteristic(Characteristic.FirmwareRevision, newAccessory.context.firmware)
     .setCharacteristic(Characteristic.SerialNumber,     newAccessory.context.serial);
+  let d = (newAccessory.context.doublePress) ? 1 : 0;
   newAccessory
     .getService(Service.StatelessProgrammableSwitch)
     .getCharacteristic(Characteristic.ProgrammableSwitchEvent)
-    .setProps({minValue: 0, maxValue: 1, validValues: [0, 1]});
+    .setProps({minValue: 0, maxValue: d, validValues: [0, d]});
   self.accessories[newAccessory.context.mac] = newAccessory;
   self.alias[newAccessory.context.mac] = newAccessory.context.mac; // self-referential
   if (self.debug >= 2) { self.log(`\x1b[4;97m${button.name}\x1b[0m added as ${button.MAC}`); }
